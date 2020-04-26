174ece16ff5d16b0271dcac0c3030d0e1677ae5e
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-04-27 02:11:04 +0300

    Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = process.env.token;
+
+bot.on('ready', () =>{
+    
+    console.log('This bot is Online!');
+})
+
+bot.on('message', msg=>{
+    
+    if(msg.content === "!credit"){
+        msg.reply('Dev by ! ᴘsʏᴄʜᴏ ⚡#4863');
+    }
+})
+
+
+bot.login(token);
