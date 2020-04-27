4e3bfb437d1352c41bd959798146fb2435871534
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-04-27 14:13:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const token = process.env.token;
 bot.on('ready', () =>{
     
     console.log('This bot is Online!');
-    bot.user.setActivity('PsychO | !credit');
+    bot.user.setActivity('! ᴘsʏᴄʜᴏ ⚡#4863 | !credit');
 })
 
 bot.on('message', msg=>{
     
     if(msg.content === "!credit"){
         msg.reply('Dev by ! ᴘsʏᴄʜᴏ ⚡#4863');
+    }
+})
+
+bot.on('message', msg=>{
+    
+    if(msg.content === "נודר"){
+        msg.channel.sendMessage('נדרים');
     }
 })
 
