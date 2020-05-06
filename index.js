836f0499a6f0cd8ae547b2e84143b38e85d168ff
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-07 00:02:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 bot.on('ready', () =>{
     
     console.log('This bot is Online!');
-    bot.user.setActivity('! ᴘsʏᴄʜᴏ ⚡#4863 | !credit') ;
+    bot.user.setActivity('Version: 1.0.7 | !credit') ;
 })
 
 
@@ -34,6 +34,20 @@ bot.on('message', msg=>{
     
     if(msg.content === "חן בסט"){
         msg.reply('http://prntscr.com/s6vcm0');
+    }
+})
+
+bot.on('message', msg=>{
+    
+    if(msg.content === "שאליקו"){
+        msg.reply('https://cdn.discordapp.com/attachments/639192344591073321/707697888918110218/Screenshot_939.png');
+    }
+})
+
+bot.on('message', msg=>{
+    
+    if(msg.content === "מישל"){
+        msg.reply('https://cdn.discordapp.com/attachments/693792348907503686/707698166769909861/2371528-46.png');
     }
 })
 
