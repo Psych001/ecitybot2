5763535a07deea4ad99923cd57cd7bc1c055d950
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-08 15:50:15 +0300

    Update

--- index.js
+++ index.js
@@ -5,73 +5,30 @@ const bot = new Discord.Client();
 bot.on('ready', () =>{
     
     console.log('This bot is Online!');
-    bot.user.setActivity('Version: 1.0.8 | !credit') ;
 })
 
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "!credit"){
-        msg.reply('Dev by ! ᴘsʏᴄʜᴏ ⚡#4863');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "אוהד"){
-        msg.reply('המלך שלי ♥♥♥♥♥♥');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "פרינס"){
-        msg.reply('הפלופר');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "חן בסט"){
-        msg.reply('http://prntscr.com/s6vcm0');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "מוריס"){
-        msg.reply('מוריס מת');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "שאליקו"){
-        msg.reply('http://prntscr.com/sc73qj');
-    }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "מישל"){
-        msg.reply('https://cdn.discordapp.com/attachments/693792348907503686/707698166769909861/2371528-46.png');
-    }
-})
-
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "קווין"){
-        msg.reply('https://cdn.discordapp.com/attachments/458277615011364864/707701222269911110/unknown.png');
-    }
-})
 
 bot.on('message', msg=>{
     
     if(msg.content === "!version"){
-        msg.reply('Version: 1.0.8');
+        msg.reply('Version: 1.0.9');
     }
 })
+
+
+function activity() {
+  setTimeout(() => {
+    fivereborn.query(configs.serverInfo[0], configs.serverInfo[1], (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        client.user.setActivity(" online " + data.clients + "/" + data.maxclients, { type: configs.activityType });
+      }
+    });
+    activity();
+  }, 10000);
+}
+activity();
 
 
 bot.login(token);
