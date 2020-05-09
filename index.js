a54325d816c631792f7d71ceb98ae18145eda1b2
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-09 21:12:15 +0300

    dd

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const config = require("./config.json");
+
+
 const token = process.env.token;
-client.config = config;
 
 bot.on('ready', () =>{
     
     console.log('This bot is Online!');
+    bot.user.setActivity('! ᴘsʏᴄʜᴏ ⚡#4863 | !credit') ;
+})
+
+bot.on('message', msg=>{
+    
+    if(msg.content === "!credit"){
+        msg.reply('Dev by ! ᴘsʏᴄʜᴏ ⚡#4863');
+    }
 })
 
 
-function activity() {
-  setTimeout(() => {
-    fivereborn.query(config.serverInfo[0], config.serverInfo[1], (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        client.user.setActivity(" online " + data.clients + "/" + data.maxclients, { type: config.activityType });
-      }
-    });
-    activity();
-  }, 10000);
-}
-activity();
 
 
-bot.login(config,token);
+
+bot.login(configs,token);
