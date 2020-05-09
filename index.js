59d176b79601404627344579af5b30039017d5c2
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-09 20:14:11 +0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const token = process.env.token;
 bot.on('ready', () =>{
     
     console.log('This bot is Online!');
-    bot.user.setActivity('Version 1.0.9 | !credit') ;
+    bot.user.setActivity('Version 1.1.0 | !credit') ;
 })
 
 bot.on('message', msg=>{
@@ -20,27 +20,22 @@ bot.on('message', msg=>{
 bot.on('message', msg=>{
     
     if(msg.content === "!version"){
-        msg.reply('1.0.9');
+        msg.reply('1.1.0');
         }
 })
 
-fivereborn.query("51.75.73.161", 30120, (err, data) => {
-  if (!err) {
-    console.log(data)
-  } else {
-    console.log('Server is offline.')
-  }
-})
-
-bot.on('message', msg=>{
-    
-    if(msg.content === "!data"){
-        msg.reply(data);
-        }
-})
-
-
-
-    
+function activity() {
+  setTimeout(() => {
+    fivereborn.query(configs.serverInfo[0], configs.serverInfo[1], (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        client.user.setActivity(" online " + data.clients + "/" + data.maxclients, { type: configs.activityType });
+      }
+    });
+    activity();
+  }, 10000);
+}
+ 
 
 bot.login(token);
