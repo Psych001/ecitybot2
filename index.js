477510d375b3f44da70c442ad2ceee78d5dcc852
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-09 20:36:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const configs = require("./configs.json");
 const fivereborn = require('fivereborn-query')
-client.config = configs;
+// client.config = configs;
 
 
 const token = process.env.token;
