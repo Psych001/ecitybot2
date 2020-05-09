3013f5d446deb033d67535cb6deebc4f6c6c9cca
Psych001 <41246230+Psych001@users.noreply.github.com>
2020-05-09 19:40:13 +0300

    Update

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const fivereborn = require('fivereborn-query')
 
-=======
-/////////////////////////////////////////////////////
-// VARIABLES
-/////////////////////////////////////////////////////
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const configs = require("./configs.json");
-const fivereborn = require('fivereborn-query');
->>>>>>> 9fd8aac72c44eb9f4c9ea29e6b0baf09b43c5786
 const token = process.env.token;
-client.config = configs;
-
-/////////////////////////////////////////////////////
-// START THE BOT
-/////////////////////////////////////////////////////
 
 bot.on('ready', () =>{
     
@@ -26,7 +9,6 @@ bot.on('ready', () =>{
     bot.user.setActivity('! ᴘsʏᴄʜᴏ ⚡#4863 | !credit') ;
 })
 
-<<<<<<< HEAD
 bot.on('message', msg=>{
     
     if(msg.content === "!credit"){
@@ -34,33 +16,28 @@ bot.on('message', msg=>{
     }
 })
 
-fivereborn.query("51.75.73.161", 30120, (err, data) => {
-  if (!err) {
-    console.log(data)
-  } else {
-    console.log('Server is offline.')
-  }
+bot.on('message', msg=>{
+    
+    if(msg.content === "אוהד"){
+        msg.reply('המלך שלי ♥♥♥♥♥♥');
+    }
 })
 
-=======
-/////////////////////////////////////////////////////
-// FUNCTIONS
-/////////////////////////////////////////////////////
+bot.on('message', msg=>{
+    
+    if(msg.content === "פרינס"){
+        msg.reply('הפלופר');
+    }
+})
 
-function activity() {
-  setTimeout(() => {
-    fivereborn.query(configs.serverInfo[0], configs.serverInfo[1], (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        client.user.setActivity(" online " + data.clients + "/" + data.maxclients, { type: configs.activityType });
-      }
-    });
-    activity();
-  }, 10000);
-}
-activity();
->>>>>>> 9fd8aac72c44eb9f4c9ea29e6b0baf09b43c5786
+bot.on('message', msg=>{
+    
+    if(msg.content === "חן בסט"){
+        msg.reply('http://prntscr.com/s6vcm0');
+    }
+})
+
+
 
 
 bot.login(token);
